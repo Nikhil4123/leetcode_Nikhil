9cee42beeb83e497aff36c0de1b2d6ac9c522040
Nikhil Sarak <140052244+Nikhil4123@users.noreply.github.com>
2025-08-17 00:14:14 +0530

    Time: 1 ms (99.54%) | Memory: 45 MB (73.21%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,26 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        int mx = 0;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0}); // index = 0, jumps = 0
-
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int i = curr[0], d = curr[1];
-
-            if (i == n - 1) return d; // reached last index
-
-            int h = Math.min(n - 1, i + nums[i]); // max jump from i
-            int l = Math.max(i + 1, mx + 1);      // start from new unexplored index
-
-            for (int k = l; k <= h; k++) {
-                q.add(new int[]{k, d + 1});
-                mx = k; // mark as visited up to k
+        int jumps = 0, currentEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
             }
         }
-        return -1;
+        return jumps;
     }
 }
