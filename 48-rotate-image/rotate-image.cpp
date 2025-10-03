2d09f94117825bcb7bae7d1f2054c45c5c5a2e85
Nikhil Sarak <140052244+Nikhil4123@users.noreply.github.com>
2025-10-03 22:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (35.53%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = t;
+            }
+        }
+    }
+};
