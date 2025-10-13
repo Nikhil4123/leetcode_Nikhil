b7b1c2901e5ebecbad0c473813dbecb1eb30bdf8
Nikhil Sarak <140052244+Nikhil4123@users.noreply.github.com>
2025-10-13 22:43:34 +0530

    Time: 7 ms (14.17%) | Memory: 45.3 MB (11.81%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -13,33 +13,34 @@ class Solution {
         return rs;
     }
 
-    private boolean isAnagrams(String s1, String s2) {
-    if (s1.length() != s2.length()) return false;
-    int[] freq = new int[26];
-    for (int i = 0; i < s1.length(); i++) {
-        freq[s1.charAt(i) - 'a']++;
-        freq[s2.charAt(i) - 'a']--;
-    }
-    for (int count : freq) {
-        if (count != 0) return false;
-    }
-    return true;
-}
-
-
-    // public boolean isAnagrams(String s1, String s2) {
-
+    // private boolean isAnagrams(String s1, String s2) {
     //     if (s1.length() != s2.length())
     //         return false;
-
-    //     HashMap<Character, Integer> h1 = new HashMap<>();
-    //     HashMap<Character, Integer> h2 = new HashMap<>();
+    //     int[] freq = new int[26];
     //     for (int i = 0; i < s1.length(); i++) {
-    //         h1.put(s1.charAt(i), h1.getOrDefault(s1.charAt(i), 0) + 1);
+    //         freq[s1.charAt(i) - 'a']++;
+    //         freq[s2.charAt(i) - 'a']--;
     //     }
-    //     for (int i = 0; i < s2.length(); i++) {
-    //         h2.put(s2.charAt(i), h2.getOrDefault(s2.charAt(i), 0) + 1);
+    //     for (int count : freq) {
+    //         if (count != 0)
+    //             return false;
     //     }
-    //     return h1.equals(h2);
+    //     return true;
     // }
+
+    public boolean isAnagrams(String s1, String s2) {
+
+        if (s1.length() != s2.length())
+            return false;
+
+        HashMap<Character, Integer> h1 = new HashMap<>();
+        HashMap<Character, Integer> h2 = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            h1.put(s1.charAt(i), h1.getOrDefault(s1.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            h2.put(s2.charAt(i), h2.getOrDefault(s2.charAt(i), 0) + 1);
+        }
+        return h1.equals(h2);
+    }
 }
