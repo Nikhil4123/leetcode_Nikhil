6c9edff22e30a5332b9c538685e602faa0ba5931
Nikhil Sarak <140052244+Nikhil4123@users.noreply.github.com>
2025-10-13 22:35:58 +0530

    Time: 7 ms (14.17%) | Memory: 45.4 MB (6.30%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> rs = new ArrayList<>();
+        rs.add(words[0]);
+        for(int i=1;i<words.length;i++){
+            if(!isAnagrams(words[i-1],words[i])){
+                rs.add(words[i]);
+            }
+        }
+        return rs;
+    }
+    public boolean isAnagrams(String s1, String s2){
+        HashMap<Character, Integer> h1 = new HashMap<>();
+        HashMap<Character, Integer> h2 = new HashMap<>();
+        for(int i=0;i<s1.length();i++){
+            h1.put(s1.charAt(i),h1.getOrDefault(s1.charAt(i),0)+1);
+        }
+        for(int i=0;i<s2.length();i++){
+            h2.put(s2.charAt(i),h2.getOrDefault(s2.charAt(i),0)+1);
+        }
+        return h1.equals(h2);
+    }
+}
