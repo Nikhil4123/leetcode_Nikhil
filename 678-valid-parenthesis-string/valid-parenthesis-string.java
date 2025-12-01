7f17d7db758099ecb23fa0808fb5831564083307
Nikhil Sarak <140052244+Nikhil4123@users.noreply.github.com>
2025-12-01 12:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (51.18%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean checkValidString(String s) {
         int low = 0, high = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
                 low++;
                 high++;
-            } else if (c == ')') {
-                low--;
+            } else if (ch == ')') {
+                if (low > 0) low--;
                 high--;
-            } else { // '*'
-                low--;
+            } else {
+                if (low > 0) low--;
                 high++;
             }
             if (high < 0) return false;
-            if (low < 0) low = 0;
         }
         return low == 0;
     }
