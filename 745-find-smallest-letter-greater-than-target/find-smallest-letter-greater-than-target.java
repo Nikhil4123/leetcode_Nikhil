7c46b01fe80f0ad6d17669495564aa0b47e0345a
Nikhil Sarak <140052244+Nikhil4123@users.noreply.github.com>
2026-02-01 00:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (9.86%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -1,13 +1,11 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        char ans = '{'; // character just after 'z'
-
-        for (char ch : letters) {
-            if (ch > target && ch < ans) {
-                ans = ch;
+        char ans='{';
+        for(char ch:letters){
+            if(ch>target &&ch<ans){
+                ans=ch;
             }
         }
-
-        return ans == '{' ? letters[0] : ans;
+        return ans=='{'?letters[0]:ans;
     }
 }
