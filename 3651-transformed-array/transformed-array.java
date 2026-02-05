ba7bd925f8e15bf23f85305fe59849b213a8493d
Nikhil Sarak <140052244+Nikhil4123@users.noreply.github.com>
2026-02-05 23:57:03 +0530

    Time: 1 ms (98.72%) | Memory: 46.8 MB (30.77%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -1,22 +1,13 @@
 class Solution {
-    public int[] constructTransformedArray(int[] A) {
-        int n = A.length;
-        int OFFSET = 1 << 7;
-        int MASK = (1 << 8) - 1;
-
-        for (int i = 0; i < n; i++)
-            A[i] += OFFSET;
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
 
         for (int i = 0; i < n; i++) {
-            int cur = A[i] - OFFSET;
-            int idx = ((i + cur) % n + n) % n;
-            int val = A[idx] & MASK;
-            A[i] |= val << 8;
+            int idx = (i + nums[i]) % n;
+            if (idx < 0) idx += n;
+            ans[i] = nums[idx];
         }
-
-        for (int i = 0; i < n; i++)
-            A[i] = (A[i] >> 8) - OFFSET;
-
-        return A;
+        return ans;
     }
 }
